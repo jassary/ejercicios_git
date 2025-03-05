3eed016511ea1e4c1749b862dc8988bc18db5139
jassary <jassaryespinoza@gmail.com>
2025-03-04 22:23:23 -0600

    tareas

---
+++ conversor/ejercicio_5.py
@@ -0,0 +1,7 @@
+#CONERSOR DE FORMATO DE NOMBRE
+
+#Pide al usuario su nombre completo en minisculas y conviertelo a formato de titulo (iniciales en mayusculas)
+
+nombre=input('ingrese su nombre completo: ')
+nombre_correcto= nombre.title()
+print (nombre_correcto)
